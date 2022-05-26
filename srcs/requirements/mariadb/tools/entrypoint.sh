44cc3e485bd9a59ece03aad8b268e651f8d68c04
user42 <dkoriaki@student.42.fr>
2022-05-27 01:01:36 +0200

    va te faire foutre

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-chmod o+w /dev/stdout
-chmod o+w /dev/stderr
+echo "1 = ${MYSQL_ROOT_PASSWORD}"
 
-if [ "$MYSQL_ROOT_PASSWORD" = "" ] || [ "$MYSQL_USER" = "" ] || [ "$MYSQL_DATABASE" = "" ]; then
-	echo "Error: some environment variables are not set"
+if [ "${MYSQL_ROOT_PASSWORD}" = "" ] || [ "${MYSQL_ROOT}" = "" ] || [ "${DB_NAME}" = "" ]; then
+	echo "Error: some environment variables are not set for mariadb installation"
 	exit 1
 fi
 
 if [ ! -d /var/lib/mysql/$DB_NAME ] ; then
 
 	echo "Creating the $DB_NAME database..."
-	mysqld&
+	service mysql start
+	echo "CACA"
 	until mysqladmin ping;
 	do
 		sleep 2
 	done
+	echo "PIPI"
 	mysql -u root -e "CREATE DATABASE $DB_NAME;"
-    	mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
+    mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
    	mysql -u root -e "CREATE USER '$MYSQL_ROOT'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-    	mysql -u root -e "GRANT ALL ON *.* TO '$DB_ROOT'@'%';"
-    	mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
-    	mysql -u root -e "GRANT ALL ON db_wordpress.* TO '$MYSQL_USER'@'%';"
+    mysql -u root -e "GRANT ALL ON *.* TO '$DB_ROOT'@'%';"
+	mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+   	mysql -u root -e "GRANT ALL ON db_wordpress.* TO '$MYSQL_USER'@'%';"
    	mysql -u root -e "FLUSH PRIVILEGES;"
 	mysql -e "DELETE FROM mysql.user WHERE user=''"
 	mysql -e "DELETE FROM mysql.user WHERE user='root'"
 	mysql -e "FLUSH PRIVILEGES"
+	echo "PIPIPOPO"
 	killall mysqld
-	echo "End first run"
-	else
-		echo "database already created"
+	echo "$DB_NAME database has been created"
+else
+	echo "$DB_NAME database already created"
 fi
 
+echo "J'ai envie de tout nqiuer"
+
+service mysql start
